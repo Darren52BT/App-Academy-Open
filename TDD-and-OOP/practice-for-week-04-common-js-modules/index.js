c30c2cac25cb0a857864c636236448dc8ef19b26
DarrenBT <darrenbt52@gmail.com>
2022-12-21 19:13:34 -0800

    finished practice with import and export of common JS modules

---
+++ TDD-and-OOP/practice-for-week-04-common-js-modules/index.js
@@ -0,0 +1,12 @@
+
+// Your code here
+const sayHelloTo = require("./send-messages/say-hello-to.js");
+const giveMessageToMrsPotato = require("./send-messages/give-message-to-mrs-potato.js")
+const {msg1, msg2, msg3} = require("./messages")
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+sayHelloTo("Mr. Potato");
+giveMessageToMrsPotato(msg1);
+giveMessageToMrsPotato(msg2);
+giveMessageToMrsPotato(msg3);
