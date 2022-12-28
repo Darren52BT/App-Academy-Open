f11c13f0f7991f5c6bb5753293a1e671dcfe6def
DarrenBT <darrenbt52@gmail.com>
2022-12-28 02:37:18 -0800

    finished long practice for context

---
+++ TDD-and-OOP/practice-for-week-04-context-long-practice/problems/10-party-planner.js
@@ -0,0 +1,28 @@
+// Your code here
+class PartyPlanner{
+	constructor(guestList =[])
+	{
+		this.guestList = guestList;
+	}
+	addToGuestList(name){
+		this.guestList.push(name);
+		return this.guestList;
+	}
+	throwParty(){
+		if(this.guestList.length===0){
+			return "Gotta add people to the guest list";
+		} else{
+			return `Welcome to the party ${this.guestList.join(" and ")}`;
+		}
+	}
+}
+
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+	module.exports = PartyPlanner;
+} catch {
+	module.exports = null;
+}
